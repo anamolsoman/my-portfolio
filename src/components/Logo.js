4ac1f3bb08a328e6d2d5c2dba9415371c31f5875
Anamol Soman <anamolsoman2@gmail.com>
2024-07-04 08:43:01 +0530

    Updated the home page.

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,22 +1,30 @@
 import React from "react";
 import Link from "next/link";
 
-// import { motion } from "framer-motion";
+import { motion } from "framer-motion";
 
 // const MotionLink = motion(Link);
 
 const Logo = () => {
   return (
     <div className="flex">
-      <Link
-        href="/"
+      <motion.a
+        href="https://google.com"
         className="bg-dark flex w-16 h-16 text-light items-center justify-center rounded-full font-bold text-2xl"
         whileHover={{
-          scale: 1.5,
+          scale: 1.2,
+          backgroundColor: [
+            "#121212",
+            "rgba(131,58,180,1)",
+            "rgba(253,29,29,1)",
+            "rgba(252,176,69,1)",
+            "rgba(131,58,180,1)",
+            "#121212",
+          ],
         }}
       >
         AS
-      </Link>
+      </motion.a>
     </div>
   );
 };
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,22 @@
 import React from "react";
 import Link from "next/link";
 import Logo from "./Logo";
-
+import { useRouter } from "next/router";
+import { GithubIcon, LinkedInIcon, PinterestIcon, TwitterIcon } from "./Icons";
+import { motion } from "framer-motion";
 const CustomLink = ({ href, title, className }) => {
+  const router = useRouter();
   return (
-    <Link href={href} className={`${className}`}>
+    <Link href={href} className={`${className} relative group `}>
       {title}
+
+      <span
+        className={`h-[1px] inline-block  bg-dark absolute  left-0 -bottom-0.5 group-hover:w-full transition-[width] ease duration-300 ${
+          router.asPath === href ? "w-full" : "w-0"
+        }`}
+      >
+        &nbsp;
+      </span>
     </Link>
   );
 };
@@ -20,22 +31,43 @@ const Navbar = () => {
         <CustomLink href="/articles" title="Articles" className="ml-4" />
       </nav>
 
-      <nav>
-        <Link href="/" target={"_blank"}>
-          T
-        </Link>{" "}
-        <Link href="/" target={"_blank"}>
-          T
-        </Link>{" "}
-        <Link href="/" target={"_blank"}>
-          T
-        </Link>{" "}
-        <Link href="/" target={"_blank"}>
-          T
-        </Link>{" "}
-        <Link href="/" target={"_blank"}>
-          T
-        </Link>
+      <nav className="flex items-center justify-center flex-wrap">
+        <motion.a
+          href="https://twitter.com"
+          target={"_blank"}
+          className="w-6 mx-3"
+          whileHover={{ y: -2 }}
+          whileTap={{ scale: 0.9 }}
+        >
+          <TwitterIcon />
+        </motion.a>{" "}
+        <motion.a
+          href="https://twitter.com"
+          target={"_blank"}
+          className="w-6 mx-3"
+          whileHover={{ y: -2 }}
+          whileTap={{ scale: 0.9 }}
+        >
+          <GithubIcon />
+        </motion.a>{" "}
+        <motion.a
+          href="https://twitter.com"
+          target={"_blank"}
+          className="w-6 mx-3"
+          whileHover={{ y: -2 }}
+          whileTap={{ scale: 0.9 }}
+        >
+          <LinkedInIcon />
+        </motion.a>{" "}
+        <motion.a
+          href="https://twitter.com"
+          target={"_blank"}
+          className="w-6 ml-3"
+          whileHover={{ y: -2 }}
+          whileTap={{ scale: 0.9 }}
+        >
+          <PinterestIcon />
+        </motion.a>{" "}
       </nav>
       <div className="absolute left-[50%] top-4 translate-x-[-50%]">
         <Logo />
