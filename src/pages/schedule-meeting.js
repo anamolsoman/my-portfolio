2e0263bd835e72bcdbdc7102a8fc0073f1c434b4
Anamol Soman <anamolsoman2@gmail.com>
2024-07-18 07:30:38 +0530

    Transition effect issue fixed.

--- src/pages/schedule-meeting.js
+++ src/pages/schedule-meeting.js
@@ -1,11 +1,13 @@
 import Layout from "@/components/Layout";
 import ScheduleMeeting from "@/components/ScheduleMeeting";
+import TransitionEffect from "@/components/TransitionEffect";
 import React from "react";
 
 export default function meetings() {
   return (
     <div>
       <Layout className="p-10 pt-0">
+        <TransitionEffect />
         <ScheduleMeeting />
       </Layout>
     </div>
