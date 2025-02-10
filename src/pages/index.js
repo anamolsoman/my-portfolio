efc07736c10e56558bbc01ee84ac79d67cf18ffe
Anamol Soman <anamolsoman2@gmail.com>
2025-02-10 21:58:50 +0530

    New Project added.

--- src/pages/index.js
+++ src/pages/index.js
@@ -34,7 +34,6 @@ export default function Home() {
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
-
               gtag('config', 'G-YSQ6QJPRS8');
             `,
           }}
